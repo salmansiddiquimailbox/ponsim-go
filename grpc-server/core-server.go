4dbd01f8cfe4900a4280eced55003773fc319c0c
Girish Gowdru <ggowdru@radisys.com>
2018-10-25 06:31:26 -0700

    TODO Work in progress

---
+++ grpc-server/core-server.go
@@ -0,0 +1,41 @@
+package grpc_server
+
+import (
+	"log"
+	"net"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org"
+	"sync"
+	pb "openolt"
+)
+
+const (
+	port = "localhost:50060"
+)
+
+func StartGRPCServer(){
+	var w sync.WaitGroup
+	log.Println("GRPC Server")
+	lis,err:=net.Listen("tcp",port)
+	if err!=nil{
+		log.Fatalf("Unable to listen to this IP and Port\nProbably the port is already in use")
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterOpenoltServer(s, &Server{})
+
+	reflection.Register(s)
+
+	w.Add(1)
+
+	go func(){
+		defer w.Done()
+		log.Println("Starting gRPC Server")
+		er:=s.Server(lis)
+		if er!=nil{
+		log.Fatalf("Error! Unable to server...")
+		}
+	}
+	w.Wait()
+}
